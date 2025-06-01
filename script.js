497e04207b45b3861c3037ff569bed36ebec6274
Simona Šusterová <simsusterova@gmail.com>
2025-06-01 09:13:39 +0200

    animace

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+//rotace kostky
+  const iceImage = document.querySelector('.ice');
+
+  setInterval(() => {
+    iceImage.classList.add('rotate-once');
+
+  
+    setTimeout(() => {
+      iceImage.classList.remove('rotate-once');
+    }, 1000); 
+  }, 5000);
+  
